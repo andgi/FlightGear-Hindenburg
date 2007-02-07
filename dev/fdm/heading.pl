c199e7ae866ce048d420b6af8edad43a9032e42e
Anders Gidenstam <anders@gidenstam.org>
2007-02-07 23:49:28 +0000

    Some more updates. Significant improvment in AP yaw stability.

--- dev/fdm/heading.pl
+++ dev/fdm/heading.pl
@@ -5,10 +5,10 @@ set ylabel "(various)"
 set xlabel "Time (sec)"
 plot \
      "LZ-129_datalog.csv" using 1:54 title "Heading (degrees)"\
-    ,"LZ-129_datalog.csv" using 1:52 title "Rudder position (rad)"\
-    ,"LZ-129_datalog.csv" using 1:51 title "Rudder position (norm.)"\
-    ,"LZ-129_datalog.csv" using 1:139 title "Heading Setpoint (degrees)"\
+    ,"LZ-129_datalog.csv" using 1:53 title "Rudder position (norm.)"\
+    ,"LZ-129_datalog.csv" using 1:107 title "Heading Setpoint (degrees)"\
     ,"LZ-129_datalog.csv" using 1:57 title "Heading Error (degrees)"\
+    ,"LZ-129_datalog.csv" using 1:43 title "Beta (degrees)"\
 #    ,"LZ-129_datalog.csv" using 1:58 title "Heading AP PID"\
 #    ,"LZ-129_datalog.csv" using 1:58 title "Heading AP Linear"\
 #    ,"LZ-129_datalog.csv" using 1:59 title "Heading AP Integrator"\
--- dev/fdm/pitch.pl
+++ dev/fdm/pitch.pl
@@ -4,10 +4,11 @@ set title  "Pitch AP"
 set ylabel "(various)"
 set xlabel "Time (sec)"
 plot \
-     "LZ-129_datalog.csv" using 1:40 title "Theta (pitch)"\
+     "LZ-129_datalog.csv" using 1:40 title "Theta (pitch deg)"\
+    ,"LZ-129_datalog.csv" using 1:42 title "Alpha (deg)"\
     ,"LZ-129_datalog.csv" using 1:50 title "Elevator position"\
-    ,"LZ-129_datalog.csv" using 1:61 title "Pitch setpoint (rad)"\
-    ,"LZ-129_datalog.csv" using 1:62 title "Pitch error (rad)"\
+    ,"LZ-129_datalog.csv" using 1:63 title "Pitch setpoint (rad)"\
+    ,"LZ-129_datalog.csv" using 1:64 title "Pitch error (rad)"\
 #    ,"LZ-129_datalog.csv" using 1:63 title "Pitch AP Linear"\
 #    ,"LZ-129_datalog.csv" using 1:64 title "Pitch AP Integrator"\
 #    ,"LZ-129_datalog.csv" using 1:64 title "Pitch AP Derivator"\
